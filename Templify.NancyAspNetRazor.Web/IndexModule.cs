e56ceccd9e92e85b7c9143dc8fbddc402e4172d3
Brendan McMahon <bernos@gmail.com>
2014-03-09 21:44:46 +1100

    added layout

---
+++ Templify.NancyAspNetRazor.Web/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace Templify.NancyAspNetRazor.Web
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
