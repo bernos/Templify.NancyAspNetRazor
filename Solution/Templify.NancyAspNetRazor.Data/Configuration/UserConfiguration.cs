b9e3ebac0f050eee8b815aa598dc4741ffbee496
Brendan McMahon <brendan.mcmahon@tigerpistol.com.au>
2014-05-30 22:06:10 +1000

    fixed version format string. will   need to manually increment version nums in AssemblyInfo.cs from now on. rm old versions of package

--- Solution/Templify.NancyAspNetRazor.Data/Configuration/UserConfiguration.cs
+++ Solution/Templify.NancyAspNetRazor.Data/Configuration/UserConfiguration.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
+using Templify.NancyAspNetRazor.Data.Models;
 
 namespace Templify.NancyAspNetRazor.Data.Configuration
 {
