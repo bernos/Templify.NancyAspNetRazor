dab9b41fe4dfc6b631d0c271ef03f6e5102ccc9e
Brendan McMahon <bernos@gmail.com>
2014-05-31 15:10:35 +1000

    improved templify build process. nuget packages no longer come along for the ride. working on getting default .gitattributes and .gitignore files included in templify package

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%systemroot%\Microsoft.Net\Framework64\v4.0.30319\MSBuild.exe build.proj /t:Templify /p:Configuration=Release /p:VisualStudioVersion=12.0
+%systemroot%\Microsoft.Net\Framework64\v4.0.30319\MSBuild.exe build.proj /p:Configuration=Release /p:VisualStudioVersion=12.0
 
 
 pause
